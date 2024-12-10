642ae60561751b6d3827679fddc9a470b6834d5b
Qwazilio <fghjrsdvh@gmail.com>
2024-12-10 14:17:25 +0300

    update export to xslx

--- frontend/src/components/TerminalExport.tsx
+++ frontend/src/components/TerminalExport.tsx
@@ -7,8 +7,21 @@ interface TerminalExport{
 export default function TerminalExport ({terminals } : TerminalExport ) {
 
     const ExportToXSLS = () => {
+        const exportData = terminals.map((terminal) => ({
+            "Название" : terminal.name_terminal,
+            "Адрес" : terminal.address,
+            "Рег. Номер" : terminal.reg_number,
+            "ККН" : terminal.uid_terminal,
+            "Доп. инф." : terminal.comment,
+            "Организация" : terminal.organization,
+            "ФН" : terminal.active_card.uid_card,
+            "Дата" : terminal.active_card.end_date_card,
+            "Комментарий" : terminal.notification
+        }))
+        console.log(terminals)
+        console.log(exportData)
         const fileName = `monitoring_ofd ${Date.now()}`
-        const worksheet = XLSX.utils.json_to_sheet(terminals);
+        const worksheet = XLSX.utils.json_to_sheet(exportData);
         const workbook = XLSX.utils.book_new();
         XLSX.utils.book_append_sheet(workbook, worksheet, `monitoring_ofd ${Date.now()}`);
     
