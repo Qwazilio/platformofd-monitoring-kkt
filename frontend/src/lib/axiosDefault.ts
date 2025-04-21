933d9aaf4a2c2e2ccfc37638e8cb6f58e2baa563
Qwazilio <fghjrsdvh@gmail.com>
2025-04-21 10:14:02 +0300

    ??

--- frontend/src/lib/axiosDefault.ts
+++ frontend/src/lib/axiosDefault.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosDefault= axios.create({
-    baseURL: process.env.BACKEND_SERVER_URL,
+    baseURL: `${process.env.BACKEND_SERVER_URL}`,
 });
 
 axiosDefault.interceptors.request.use((config) => {
