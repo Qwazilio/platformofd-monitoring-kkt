78eaf592393645340e4ec0b9184326952988bbb8
Qwazilio <fghjrsdvh@gmail.com>
2025-04-21 09:20:18 +0300

    change hosts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,19 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'node:process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  
   app.enableCors({
-    origin: [
-      'http://localhost:61707',
-      'http://kkt.masterminutka.ru',
-      'http://192.168.1.3:61707'
-    ],
+    origin: ['http://localhost:61707', process.env.HOST, process.env.IP_HOST],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
-  })
+  });
 
   await app.listen(process.env.PORT ?? 61708);
 }
