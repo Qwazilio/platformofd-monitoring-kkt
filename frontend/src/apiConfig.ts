274414d9216c78d6e7a63a93d2065c9e4d5d6955
Qwazilio <fghjrsdvh@gmail.com>
2026-03-16 12:02:56 +0300

    для примера

--- frontend/src/apiConfig.ts
+++ frontend/src/apiConfig.ts
@@ -1,9 +1,9 @@
 export const organizations = [
-    { name: "ИПК", token: ''},
-    { name: "ИПЖ", token: "" },
-    { name: "ММ", token: "" },
-    { name: "ММР", token: "" },
-    { name: "Дом Быта", token: "" },
-    { name: "НеоСервисе", token: "" },
-    { name: "Кондрашин Никита", token: "" }
+    { id: 'ipk', name: "ИПК", token: process.env.IPK_TOKEN },
+    { id: 'ipg', name: "ИПЖ", token: process.env.IPG_TOKEN },
+    { id: 'mm', name: "ММ", token: process.env.MM_TOKEN },
+    { id: 'mmr', name: "ММР", token: process.env.MMR_TOKEN },
+    { id: 'db', name: "Дом Быта", token: process.env.DB_TOKEN },
+    { id: 'neo', name: "НеоСервисе", token: process.env.NEO_TOKEN },
+    { id: 'ipk2', name: "Климакин", token: process.env.IPK2_TOKEN }
 ] as OrganizationEntity[];
