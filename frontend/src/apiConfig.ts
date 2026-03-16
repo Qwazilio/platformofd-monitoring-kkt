94ebdd090bc6ac7cd4a03785e89fe9bd879f250d
Qwazilio <fghjrsdvh@gmail.com>
2026-03-16 11:05:23 +0300

    Create apiConfig.ts

---
+++ frontend/src/apiConfig.ts
@@ -0,0 +1,9 @@
+export const organizations = [
+    { name: "ИПК", token: ''},
+    { name: "ИПЖ", token: "" },
+    { name: "ММ", token: "" },
+    { name: "ММР", token: "" },
+    { name: "Дом Быта", token: "" },
+    { name: "НеоСервисе", token: "" },
+    { name: "Кондрашин Никита", token: "" }
+] as OrganizationEntity[];
